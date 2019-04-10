b6443093cc52197c107fd737030121a687b02635
Gabriel Castilho <castilho.gabriel14@gmail.com>
2019-04-10 12:00:00 -0300

    Gerenciador de Ethernet convertido para classe
    --Criado a classe NetworkManager para manter os processos de gerenciamento de conexão contido no escopo de um objeto.
        Essa decisão foi tomada pela suspeita de que as variáveis globais e os processos que eram feitos nas funções que lidavam com a rede estariam interferindo com o resto do programa, entretanto o código tbm ficou mais organizado desse modo

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,20 @@
 #include <Arduino.h>
-
-void networkSetup();
-void postToServer(char*);
+#include <ArduinoJson.h>
+#include "network.cpp"
 
 void setup() {
+	const char RA[] = "2761234567890";
+
 	// Initialize serial if connected
 	if(Serial) {
 		Serial.begin(9600);
 	}
-	networkSetup();
 
-	char postData[] = "{\"version\":0.1,\"RA\":2761234567890,\"telemetry\":[{\"timestamp\":1552146530,\"latitude\":-22.8044635,\"longitude\":-47.3158102,\"windVelocity\":64},{\"timestamp\":1552148940,\"latitude\":-22.8044635,\"longitude\":-47.3158102,\"windVelocity\":44}]}";
-	postToServer(postData);
+	class NetworkManager connection;
+
+	char postData[] = "{\"version\": 0.1,\"RA\": 2761234567890,\"telemetry\": [{\"timestamp\": 1552146530,\"latitude\": -22.8044635,\"longitude\": -47.3158102,\"windVelocity\": 64},{\"timestamp\": 1552148940,\"latitude\": -22.8044635,\"longitude\": -47.3158102,\"windVelocity\": 44}]}";
+
+	connection.post(postData);
 }
 
 void loop() {
--- src/network.cpp
+++ src/network.cpp
@@ -1,115 +1,115 @@
 #include <Ethernet.h>
 
-// Enter a MAC address for your controller below.
-// Newer Ethernet shields have a MAC address printed on a sticker on the shield
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+class NetworkManager {
+	private:
+		// Enter a MAC address for your controller below.
+		// Newer Ethernet shields have a MAC address printed on a sticker on the shield
+		byte mac[6] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
-// if you don't want to use DNS (and reduce your sketch size)
-// use the numeric IP instead of the name for the server:
-IPAddress server(192,168,97,164);	// numeric IP for Server (no DNS)
-//char server[] = "www.google.com";	// name address for Server (using DNS)
+		// Name address for Server 
+		char server[15] = "192.168.97.164";
 
-// Set the static IP address to use if the DHCP fails to assign
-IPAddress ip(192, 168, 0, 177);
-IPAddress myDns(192, 168, 0, 1);
+		// Initialize the Ethernet client library
+		// with the IP address and port of the server
+		// that you want to connect to (port 80 is default for HTTP):
+		class EthernetClient client;
 
-// Initialize the Ethernet client library
-// with the IP address and port of the server
-// that you want to connect to (port 80 is default for HTTP):
-EthernetClient client;
+		// Variables to measure the speed
+		unsigned long beginMicros, endMicros;
+		unsigned long byteCount = 0;
+		bool printWebData = true;	// Set to true to process and print server response
+	
+	public: NetworkManager() {
+		// Set the static IP address to use if the DHCP fails to assign
+		class IPAddress ip(192, 168, 0, 177);
+		class IPAddress myDns(8, 8, 8, 8);
 
-// Variables to measure the speed
-unsigned long beginMicros, endMicros;
-unsigned long byteCount = 0;
-bool printWebData = true;	// Set to true to process and print server response
-
-void networkSetup() {
-	// start the Ethernet connection:
-	Serial.println("Initializing Ethernet with DHCP:");
-	if (Ethernet.begin(mac)) {
-		Serial.print("  DHCP assigned IP ");
-		Serial.println(Ethernet.localIP());
-		delay(1000);	// give the Ethernet shield a second to initialize
-	} else {
-		Serial.println("Failed to configure Ethernet using DHCP");
-		// Check for Ethernet hardware present
-		if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-			Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
-			while (true) {
-				delay(1); // do nothing, no point running without Ethernet hardware
+		// start the Ethernet connection:
+		Serial.println("Initializing Ethernet with DHCP:");
+		if (Ethernet.begin(mac)) {
+			Serial.print("  DHCP assigned IP ");
+			Serial.println(Ethernet.localIP());
+			delay(1000);	// give the Ethernet shield a second to initialize
+		} else {
+			Serial.println("Failed to configure Ethernet using DHCP");
+			// Check for Ethernet hardware present
+			if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+				Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+				while (true) {
+					delay(1); // do nothing, no point running without Ethernet hardware
+				}
 			}
-		}
-		if (Ethernet.linkStatus() == LinkOFF) {
-			Serial.println("Ethernet cable is not connected.");
-			while(true){
-				delay(1); // do nothing
+			if (Ethernet.linkStatus() == LinkOFF) {
+				Serial.println("Ethernet cable is not connected.");
+				while(true){
+					delay(1); // do nothing
+				}
 			}
+			// try to configure using IP address instead of DHCP:
+			Serial.println("Warning: trying to configure internet without DHCP");
+			Ethernet.begin(mac, ip, myDns);
 		}
-		// try to congifure using IP address instead of DHCP:
-		Serial.println("Warning: trying to configure internet without DHCP");
-		Ethernet.begin(mac, ip, myDns);
-	}
-}
-
-void postToServer(char *postData) {
-	Serial.print("connecting to ");
-	Serial.print(server);
-	Serial.println("...");
-
-	// if you get a connection, report back via serial:
-	if (client.connect(server, 80)) {
-		Serial.print("connected to ");
-		Serial.println(client.remoteIP());
-
-		// Make a HTTP POST request:
-		Serial.println("Sending POST data...");
-		Serial.println();
-		client.println("POST /telemetryProject/server/telemetry.php HTTP/1.1");
-		client.print("Host: ");
-		client.println(server);
-		client.println("User-Agent: Arduino/1.0");
-		client.println("Connection: close");
-		client.print("Content-Length: ");
-		client.println(strlen(postData));
-		client.println();
-		client.println(postData);
-		beginMicros = micros();
-	} else {
-		// if you didn't get a connection to the server:
-		Serial.println("connection failed");
-		return;
 	}
 
-	// Read and print incoming bytes available from the server, if any
-	if (printWebData) {
-		Serial.println("Printing server response...");
-		while(client.connected()){
-			int len = client.available();
-			if (len > 0) {
-				byte buffer[80];
-				if (len > 80) len = 80;
-				client.read(buffer, len);
-				Serial.write(buffer, len); // show in the serial monitor (slows some boards)
-				byteCount = byteCount + len;
-			}
+	public: void post(char *postData) {
+		Serial.print("connecting to ");
+		Serial.print(server);
+		Serial.println("...");
+
+		// Try to connect to the server
+		if (client.connect(server, 80)) {
+			Serial.print("connected to ");
+			Serial.println(client.remoteIP());
+
+			// Make a HTTP POST request:
+			Serial.println("Sending POST data...");
+			Serial.println();
+			client.println("POST /telemetryProject/server/telemetry.php HTTP/1.1");
+			client.print("Host: ");
+			client.println(server);
+			client.println("User-Agent: Arduino/1.0");
+			client.println("Connection: close");
+			client.print("Content-Length: ");
+			client.println(strlen(postData));
+			client.println();
+			client.println(postData);
+			beginMicros = micros();
+		} else {
+			Serial.println("connection failed");
+			return;
 		}
-		endMicros = micros();
-		Serial.println("\n");
-		Serial.print("Received ");
-		Serial.print(byteCount);
-		Serial.print(" bytes in ");
-		float seconds = (float)(endMicros - beginMicros) / 1000000.0;
-		Serial.print(seconds, 4);
-		float rate = (float)byteCount / seconds / 1000.0;
-		Serial.print(", rate = ");
-		Serial.print(rate);
-		Serial.print(" kbytes/second");
-		Serial.println();
-	} else {
-		Serial.println("Printing response disabled, skipping");
-		Serial.println();
+
+		// Read and print incoming bytes available from the server, if any
+		if (printWebData) {
+			Serial.println("Printing server response...");
+			while(client.connected()){
+				int len = client.available();
+				if (len > 0) {
+					byte buffer[80];
+					if (len > 80) len = 80;
+					client.read(buffer, len);
+					Serial.write(buffer, len);
+					byteCount = byteCount + len;
+				}
+			}
+			//Print approximate connection velocity
+			endMicros = micros();
+			Serial.println("\n");
+			Serial.print("Received ");
+			Serial.print(byteCount);
+			Serial.print(" bytes in ");
+			float seconds = (float)(endMicros - beginMicros) / 1000000.0;
+			Serial.print(seconds, 4);
+			float rate = (float)byteCount / seconds / 1000.0;
+			Serial.print(", rate = ");
+			Serial.print(rate);
+			Serial.print(" kbytes/second");
+			Serial.println();
+		} else {
+			Serial.println("Printing response disabled, skipping");
+			Serial.println();
+		}
+		Serial.println("disconnecting...");
+		client.stop();
 	}
-	// if the server's disconnected, stop the client:
-	Serial.println("disconnecting...");
-	client.stop();
-}
+};
