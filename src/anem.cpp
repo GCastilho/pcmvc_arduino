da3bb764a7ed492489d609e727d98d7f5c777cb8
Gabriel Castilho <castilho.gabriel14@gmail.com>
2019-04-18 15:53:12 -0300

    Criado classe de manipulação do anemômetro
    --Criado classe para colheita de dados do anemômetro; A classe tem uma função getWindVelocity que calcula a velocidade média medida pelo anemômetro dos últimos 5 segundos e retorna como um float
    --RCT e anemômetro completamente funcionais

---
+++ src/anem.cpp
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+#define Hall sensor 2			//  Pino digital 2
+
+class Anemometro {
+	private:
+		// Constants definitions
+		const float pi = 3.14159265;	// Numero pi
+		const int period = 5000;		// Tempo de medida(miliseconds)
+		const int radius = 147;			// Raio do anemometro(mm)
+		// Variable definitions
+		static volatile unsigned int counter;	// magnet counter for sensor
+		unsigned int RPM;			// Revolutions per minute
+		float windSpeed;			// Wind speed (m/s)
+	
+	private: static void addcount() {
+		counter++;
+	}
+
+	// Set anemometer pins
+	public: Anemometro() {
+		pinMode(2, INPUT);
+		digitalWrite(2, HIGH);	//internall pull-up active
+	}
+
+	// Calcula a velocidade média dos últimos {period} milisegundos
+	public: float getWindVelocity() {
+		counter = 0;
+		attachInterrupt(0, addcount, RISING);
+		unsigned long startTime = millis();
+		while(millis() < startTime + period)
+
+		RPM = ((counter)*60)/(period/1000);
+		windSpeed = ((2 * pi * radius * RPM)/60) / 1000;
+
+		return windSpeed;
+	}
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <Arduino.h>
 #include "telemetryProject.h"
 #include "network.cpp"
+#include "anem.cpp"
 
+volatile unsigned int Anemometro::counter;
 char* buildMessage(const char*, const char*, const char*, struct telemetry[], size_t);
 char* getMessageSignature(char*, const char*);
 char* buildPostData(char*, char*);
@@ -14,17 +16,10 @@ const char RA[] = "2761234567890";
 const char latitude[] = "-22.8044635";
 const char longitude[] = "-47.3158102";
 
-void setup() {
-	// Initialize serial if connected
-	if(Serial) {
-		Serial.begin(9600);
-	}
-	Serial.println("START");
-	Serial.println("Starting RTC");
-	rtcSetup();
-	//class NetworkManager connection;
+class Anemometro anem;
 
-	/*struct telemetry telemetryArray[2];
+void jsonTest() {
+	struct telemetry telemetryArray[2];
 	telemetryArray[0].timestamp = 1552146530;
 	telemetryArray[0].windVelocity = 64;
 	telemetryArray[1].timestamp = 1552148940;
@@ -39,15 +34,32 @@ void setup() {
 
 	Serial.println(postData);
 
-	free(postData);*/
+	free(postData);
+}
+
+void setup() {
+	// Initialize serial if connected
+	if(Serial) {
+		Serial.begin(9600);
+	}
+	Serial.println("START SETUP");
+	Serial.println("Starting RTC");
+	rtcSetup();
+	//class NetworkManager connection;
+
 
 	//connection.post(message);
-	Serial.println("END");
+	Serial.println("END SETUP");
 }
 
 void loop() {
-	// do nothing forevermore:
-	while (true) {
-		delay(1);
-	}
+	Serial.print("Starting measurement...");
+	float windSpeed = anem.getWindVelocity();
+	Serial.println(" finished.");
+	Serial.print("Timestamp: ");
+	Serial.print(getEpoch32Time());
+	Serial.print("; Wind speed: ");
+	Serial.print(windSpeed);
+	Serial.println(" [m/s]");
+	delay(2000);
 }
